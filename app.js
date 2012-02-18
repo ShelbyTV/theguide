c19b52376e19a0bdf2299492288eab61169eaf05
Myles Recny <mkrecny@gmail.com>
2012-02-18 14:14:54 +0000

    defaults to port 1337

--- app.js
+++ app.js
@@ -28,5 +28,5 @@ app.configure('production', function(){
 
 app.get('/resolve', routes.resolve);
 
-app.listen(3000);
+app.listen(1337);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
